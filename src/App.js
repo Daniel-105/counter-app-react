108ed045417077e87eae4f1a67a6b8994e8cf5f7
Daniel-105 <danielpinto105@gmail.com>
2023-06-21 21:17:03 +0100

    Buttons and count are now in the right position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 // import Demo from "./Components/Demo";
-import { Box, Button, Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,39 +14,69 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <h1>Counter app</h1>
-        <h2>{count}</h2>
-        <Box display="flex" bgcolor="primary" className="buttons">
-          <Box m={2} p={2}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => setCount(count + 1)}
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems={"center"}
+          justifyContent={"center"}
+          //style={{ backgroundColor: "green" }}
+          height={"80vh"}
+        >
+          <h1>Counter app</h1>
+          <h2>{count}</h2>
+          <Box
+            //style={{ backgroundColor: "red" }}
+            flexDirection="row"
+            justifyContent={"space-around"}
+            display="flex"
+            className="buttons"
+          >
+            <Box
+              m={5}
+              p={5}
+              //style={{ backgroundColor: "orange" }}
             >
-              Increase
-            </Button>
-          </Box>
-          <Box m={2} p={2}>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => setCount(count - 1)}
-              disabled={count === 0}
+              <Button
+                variant="contained"
+                onClick={() => setCount(count + 1)}
+                style={{ fontSize: "2em" }}
+              >
+                Increase
+              </Button>
+            </Box>
+            <Box
+              m={5}
+              p={5}
+              // style={{ backgroundColor: "orange" }}
             >
-              Decrease
-            </Button>
-          </Box>
-          <Box m={2} p={2}>
-            <Button
-              variant="contained"
-              onClick={() => setCount(count - count)}
-              disabled={count === 0}
+              <Button
+                variant="contained"
+                color="secondary"
+                // style=a{{ padding: "14px" }}
+                onClick={() => setCount(count - 1)}
+                disabled={count === 0}
+                style={{ fontSize: "2em" }}
+              >
+                Decrease
+              </Button>
+            </Box>
+            <Box
+              m={5}
+              p={5}
+              //style={{ backgroundColor: "orange" }}
             >
-              Reset
-            </Button>
+              <Button
+                variant="contained"
+                onClick={() => setCount(count - count)}
+                disabled={count === 0}
+                style={{ fontSize: "2em" }}
+              >
+                Reset
+              </Button>
+            </Box>
           </Box>
+          {/* <Demo /> */}
         </Box>
-        {/* <Demo /> */}
       </Container>
     </div>
   );
